3d3a4000fccea534837a4406b323010b71cc9b4e
Gabriel Manfroi <32476134+gabemanfroi@users.noreply.github.com>
2021-03-04 15:52:58 -0300

    Salvamento

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
+    ]
 })
 export class PhotoFormModule { }
